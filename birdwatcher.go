08c22dcd72e30e51e348b3c9ad2268157ea73213
Daniel Melani <daniel@netnod.se>
2016-10-23 09:51:40 +0200

    test endpoint

--- birdwatcher.go
+++ birdwatcher.go
@@ -1,4 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"log"
+	"net/http"
+)
+
+func Test(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Fprint(w, "Test\n")
+}
+
 func main() {
+	r := httprouter.New()
+	r.GET("/test", Test)
+
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
