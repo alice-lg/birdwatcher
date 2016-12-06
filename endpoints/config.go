b6d715d6ccd4d8dfe3d2ceb32fc57be8715b5098
Matthias Hannig <matthias@hannig.cc>
2016-12-06 13:17:43 +0100

    whitelist routes

--- endpoints/config.go
+++ endpoints/config.go
@@ -2,5 +2,6 @@ package endpoints
 
 // Endpoints / Server configuration
 type ServerConfig struct {
-	AllowFrom []string `toml:"allow_from"`
+	AllowFrom      []string `toml:"allow_from"`
+	ModulesEnabled []string `toml:"modules_enabled"`
 }
