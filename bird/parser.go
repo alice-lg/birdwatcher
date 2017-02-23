9c4a47fd11b8fc2d6e0c322b55d68896f8beb5a3
hellerve <veit@veitheller.de>
2017-02-23 13:02:05 +0100

    interface names can contain periods

--- bird/parser.go
+++ bird/parser.go
@@ -145,7 +145,7 @@ func parseRoutes(input []byte) Parsed {
 	routes := []Parsed{}
 
 	route := Parsed{}
-	start_def_rx := regexp.MustCompile(`^([0-9a-f\.\:\/]+)\s+via\s+([0-9a-f\.\:]+)\s+on\s+(\w+)\s+\[([\w\.:]+)\s+([0-9\-\:\s]+)(?:\s+from\s+([0-9a-f\.\:\/]+)){0,1}\]\s+(?:(\*)\s+){0,1}\((\d+)(?:\/\d+){0,1}\).*`)
+	start_def_rx := regexp.MustCompile(`^([0-9a-f\.\:\/]+)\s+via\s+([0-9a-f\.\:]+)\s+on\s+([\w\.]+)\s+\[([\w\.:]+)\s+([0-9\-\:\s]+)(?:\s+from\s+([0-9a-f\.\:\/]+)){0,1}\]\s+(?:(\*)\s+){0,1}\((\d+)(?:\/\d+){0,1}\).*`)
 	second_rx := regexp.MustCompile(`^\s+via\s+([0-9a-f\.\:]+)\s+on\s+([\w+)\s+\[([\w\.]+)\s+([0-9\-\:\s]+)(?:\s+from\s+([0-9a-f\.\:\/]+)){0,1}\]\s+(?:(\*)\s+){0,1}\((\d+)(?:\/\d+){0,1}\).*$`)
 	type_rx := regexp.MustCompile(`^\s+Type:\s+(.*)\s*$`)
 	bgp_rx := regexp.MustCompile(`^\s+BGP.(\w+):\s+(.+)\s*$`)
--- birdwatcher.go
+++ birdwatcher.go
@@ -13,7 +13,7 @@ import (
 )
 
 //go:generate versionize
-var VERSION = "1.7.14"
+var VERSION = "1.7.15"
 
 func isModuleEnabled(module string, modulesEnabled []string) bool {
 	for _, enabled := range modulesEnabled {
