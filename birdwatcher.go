4fd0b8a5cfffa3d9188d6d044389dfc2388cd9dc
Benedikt Rudolph <benedikt.rudolph@de-cix.net>
2019-02-27 11:58:48 +0100

    Increment version, add changelog for 1.12.3

--- birdwatcher.go
+++ birdwatcher.go
@@ -16,7 +16,7 @@ import (
 )
 
 //go:generate versionize
-var VERSION = "1.12.2"
+var VERSION = "1.12.3"
 
 func isModuleEnabled(module string, modulesEnabled []string) bool {
 	for _, enabled := range modulesEnabled {
